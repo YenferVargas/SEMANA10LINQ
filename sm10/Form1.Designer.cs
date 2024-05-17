0b9f64a1005210f27ca8f5cb82f47ada1704c7ee
YenferVargas <yenifer.vargas@tecsup.edu.pe>
2024-05-17 10:25:10 -0500

    Agregar archivos de proyecto.

---
+++ sm10/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace sm10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePickerHasta = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerDesde = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridViewOrdenes = new System.Windows.Forms.DataGridView();
+            this.dataGridViewClientes = new System.Windows.Forms.DataGridView();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrdenes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerHasta
+            // 
+            this.dateTimePickerHasta.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerHasta.Location = new System.Drawing.Point(359, 258);
+            this.dateTimePickerHasta.Name = "dateTimePickerHasta";
+            this.dateTimePickerHasta.Size = new System.Drawing.Size(89, 20);
+            this.dateTimePickerHasta.TabIndex = 29;
+            this.dateTimePickerHasta.ValueChanged += new System.EventHandler(this.dateTimePickerHasta_ValueChanged);
+            // 
+            // dateTimePickerDesde
+            // 
+            this.dateTimePickerDesde.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerDesde.Location = new System.Drawing.Point(208, 255);
+            this.dateTimePickerDesde.Name = "dateTimePickerDesde";
+            this.dateTimePickerDesde.Size = new System.Drawing.Size(89, 20);
+            this.dateTimePickerDesde.TabIndex = 28;
+            this.dateTimePickerDesde.ValueChanged += new System.EventHandler(this.dateTimePickerDesde_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(307, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Hasta el";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Ordenes de compra";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Desde el";
+            // 
+            // dataGridViewOrdenes
+            // 
+            this.dataGridViewOrdenes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrdenes.Location = new System.Drawing.Point(23, 296);
+            this.dataGridViewOrdenes.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewOrdenes.Name = "dataGridViewOrdenes";
+            this.dataGridViewOrdenes.RowHeadersWidth = 51;
+            this.dataGridViewOrdenes.RowTemplate.Height = 24;
+            this.dataGridViewOrdenes.Size = new System.Drawing.Size(434, 163);
+            this.dataGridViewOrdenes.TabIndex = 24;
+            this.dataGridViewOrdenes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewOrdenes_CellContentClick);
+            // 
+            // dataGridViewClientes
+            // 
+            this.dataGridViewClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewClientes.Location = new System.Drawing.Point(22, 42);
+            this.dataGridViewClientes.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewClientes.Name = "dataGridViewClientes";
+            this.dataGridViewClientes.RowHeadersWidth = 51;
+            this.dataGridViewClientes.RowTemplate.Height = 24;
+            this.dataGridViewClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewClientes.Size = new System.Drawing.Size(434, 198);
+            this.dataGridViewClientes.TabIndex = 23;
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(73, 12);
+            this.txtCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(384, 20);
+            this.txtCliente.TabIndex = 22;
+            this.txtCliente.TextChanged += new System.EventHandler(this.txtCliente_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Cliente";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 464);
+            this.Controls.Add(this.dateTimePickerHasta);
+            this.Controls.Add(this.dateTimePickerDesde);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridViewOrdenes);
+            this.Controls.Add(this.dataGridViewClientes);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrdenes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerHasta;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDesde;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridViewOrdenes;
+        private System.Windows.Forms.DataGridView dataGridViewClientes;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ sm10/Form1.cs
@@ -0,0 +1,151 @@
+﻿using sm10.Modelo;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace sm10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FiltrarClientes();
+            txtCliente.TextChanged += new EventHandler(txtCliente_TextChanged);
+            dateTimePickerDesde.ValueChanged += new EventHandler(ActualizarOrdenesDeCompra);
+            dateTimePickerHasta.ValueChanged += new EventHandler(ActualizarOrdenesDeCompra);
+            dataGridViewClientes.SelectionChanged += new EventHandler(dataGridViewClientes_SelectionChanged);
+        }
+
+        private void FiltrarClientes()
+        {
+            string textoFiltro = txtCliente.Text.Trim().ToLower();
+            using (var BD = new NegociosEntities())
+            {
+                var clientesFiltrados = from cliente in BD.CLIENTE
+                                        where cliente.NombreCia.ToLower().Contains(textoFiltro)
+                                        select new
+                                        {
+                                            cliente.IdCliente,
+                                            cliente.NombreCia,
+                                            cliente.Direccion,
+                                            cliente.Telefono,
+                                            Pais = cliente.PAIS.NombrePais 
+                                        };
+
+                dataGridViewClientes.DataSource = clientesFiltrados.ToList();
+            }
+        }
+        private void ActualizarOrdenesDeCompra(object sender, EventArgs e)
+        {
+            if (dataGridViewClientes.SelectedRows.Count > 0)
+            {
+                string clienteId = dataGridViewClientes.SelectedRows[0].Cells["IdCliente"].Value.ToString();
+                DateTime fechaDesde = dateTimePickerDesde.Value.Date;
+                DateTime fechaHasta = dateTimePickerHasta.Value.Date;
+
+                using (var BD = new NegociosEntities())
+                {
+                    var ordenes = from pedido in BD.PEDIDO
+                                  where pedido.IdCliente == clienteId &&
+                                        pedido.FechaPedido >= fechaDesde &&
+                                        pedido.FechaPedido <= fechaHasta
+                                  orderby pedido.FechaPedido
+                                  select new
+                                  {
+                                      IdPedido = pedido.IdPedido,
+                                      Cliente = pedido.CLIENTE.NombreCia,
+                                      Fecha = pedido.FechaPedido,
+                                      Total = pedido.DETALLEPEDIDO.Sum(d => d.Cantidad * d.PrecioUnidad)
+                                  };
+
+                    dataGridViewOrdenes.DataSource = ordenes.ToList();
+                }
+            }
+            else
+            {
+                dataGridViewOrdenes.DataSource = null;
+            }
+        }
+
+
+        private void FiltrarOrdenesDeCompra()
+        {
+            if (dataGridViewClientes.SelectedRows.Count > 0)
+            {
+                string clienteId = dataGridViewClientes.SelectedRows[0].Cells["IdCliente"].Value.ToString();
+                DateTime fechaDesde = dateTimePickerDesde.Value.Date;
+                DateTime fechaHasta = dateTimePickerHasta.Value.Date;
+
+                using (var BD = new NegociosEntities())
+                {
+                    var ordenes = from pedido in BD.PEDIDO
+                                  where pedido.IdCliente == clienteId &&
+                                        pedido.FechaPedido >= fechaDesde &&
+                                        pedido.FechaPedido <= fechaHasta
+                                  orderby pedido.FechaPedido
+                                  select new
+                                  {
+                                      IdPedido = pedido.IdPedido,
+                                      Cliente = pedido.CLIENTE.NombreCia,
+                                      Fecha = pedido.FechaPedido,
+                                      Total = pedido.DETALLEPEDIDO.Sum(d => d.Cantidad * d.PrecioUnidad)
+                                  };
+
+                    dataGridViewOrdenes.DataSource = ordenes.ToList();
+                }
+            }
+            else
+            {
+                dataGridViewOrdenes.DataSource = null;
+            }
+        }
+
+        private void txtCliente_TextChanged(object sender, EventArgs e)
+        {
+            FiltrarClientes();
+        }
+
+        private void dataGridViewClientes_SelectionChanged(object sender, EventArgs e)
+        {
+            FiltrarOrdenesDeCompra();
+        }
+
+        private void dateTimePickerDesde_ValueChanged(object sender, EventArgs e)
+        {
+            FiltrarOrdenesDeCompra();
+        }
+
+        private void dateTimePickerHasta_ValueChanged(object sender, EventArgs e)
+        {
+            FiltrarOrdenesDeCompra();
+        }
+
+        private void dataGridViewClientes_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                FiltrarOrdenesDeCompra();
+            }
+        }
+
+        private void dateTimePickerDesde_KeyDown(object sender, KeyEventArgs e)
+        {
+            FiltrarOrdenesDeCompra();
+        }
+
+        private void dateTimePickerHasta_KeyDown(object sender, KeyEventArgs e)
+        {
+            FiltrarOrdenesDeCompra();
+        }
+
+        private void dataGridViewOrdenes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            FiltrarOrdenesDeCompra();
+        }
+    }
+}
